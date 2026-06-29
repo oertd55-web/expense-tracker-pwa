6f59139416120ffe9088168e8036317ac16e1b67
oertd55-web <oertd55-web@users.noreply.github.com>
2026-06-29 07:11:34 +0700

    Add PNG icons, manifest description, sw cache update for PWABuilder

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "expense-tracker-v1";
-const ASSETS = ["./index.html", "./style.css", "./app.js", "./manifest.json", "./icon.svg"];
+const CACHE = "expense-tracker-v2";
+const ASSETS = ["./index.html", "./style.css", "./app.js", "./manifest.json", "./icon.svg", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
